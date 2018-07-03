763039c94ef1861b197f6eeb6a9cd0d744a20083
Warren Macpro2 <warrenmacpro2@dads-macbook-pro.local>
2018-07-03 00:18:35 -1000

    add new item functionality

--- ToDo2/ToDoListViiewController.swift
+++ ToDo2/ToDoListViiewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ToDoListViewController: UITableViewController {
     
     
-    let itemArray = ["Find MIke", "Buy Eggs", "Destroy something"]
+    var itemArray = ["Find MIke", "Buy Eggs", "Destroy something"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,10 +47,35 @@ class ToDoListViewController: UITableViewController {
         
         //animate cell on click - not leave it gray
         tableView.deselectRow(at: indexPath, animated: true)
-        
-        
-        
+ 
     }
 
+    //MARK - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        //local Vars
+        //A text field available to pass info to
+        var catcherTextField = UITextField()
+        
+      //create an alert and pop it up
+        let alert = UIAlertController(title: "Add New ToDo2 Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the add button on our UI Alert
+            self.itemArray.append(catcherTextField.text!)
+            //Reload data into tableView
+            self.tableView.reloadData()
+        }
+        //add input text field
+        alert.addTextField { (alertTextfield) in
+            alertTextfield.placeholder = "create new item"
+            //pass value of textfield to local var
+            catcherTextField = alertTextfield
+        }
+        //pop up alert with addAaction "action" parameters set above
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
 }
 
